2dbd448ed8bb9427ff1143bc7d279c0a4bde7dc0
ioana-manghiuc <ioana-manghiuc@users.noreply.github.com>
2024-04-23 11:20:25 +0300

    Changed default camera speed.

--- flight_simulator/flight_simulator/Camera.cpp
+++ flight_simulator/flight_simulator/Camera.cpp
@@ -58,11 +58,11 @@ void Camera::Inputs(GLFWwindow* window)
 	}
 	if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
 	{
-		speed = 0.01f;
+		speed = 0.1f;
 	}
 	else if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE)
 	{
-		speed = 0.001f;
+		speed = 0.01f;
 	}
 
 	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
