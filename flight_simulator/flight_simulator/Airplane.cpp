807905a1f85a99b4c7ab16898c1cd937a5470161
Mihai Moisescu <mihaimoisescu9@gmail.com>
2024-05-06 19:24:25 +0300

    changed airplane position

--- flight_simulator/flight_simulator/Airplane.cpp
+++ flight_simulator/flight_simulator/Airplane.cpp
@@ -3,7 +3,7 @@
 Airplane::Airplane():
 	model{ "models/plane/scene.gltf" }
 {
-	model.translation = glm::vec3(-95.9f, 386.8f, -8.4f);
+	model.translation = glm::vec3(-1185.9, 1386.8, 766.6);
 	model.rotation = glm::vec3(163.0f, -1171.0f, 174.55f);
 	model.scale = glm::vec3(0.1f, 0.1f, 0.1f);
 }
